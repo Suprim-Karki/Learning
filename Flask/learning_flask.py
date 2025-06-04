741272a36ab06e0ab82eacc3e3854893e28449a3
Suprim-Karki <shupreem1911@gmail.com>
2025-06-04 18:59:36 +0545

    Update functionality implemented

--- Flask/learning_flask.py
+++ Flask/learning_flask.py
@@ -45,9 +45,18 @@ def delete(id):
     
 @app.route("/update/<int:id>",methods=["POST","GET"]) 
 def update(id):
-    if request.method=="POST":
-        
+    task=Todo.query.get_or_404(id)
 
+    if request.method=="POST":
+        task.content=request.form["content"]
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Error"
+    else:
+        return render_template("update.html",task=task)
 
 if __name__=="__main__":
     with app.app_context():
