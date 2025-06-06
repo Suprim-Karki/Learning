b488b7235ffcafa40d169f0a6e70698eaa5a0629
Suprim-Karki <shupreem1911@gmail.com>
2025-06-06 17:23:25 +0545

    POST method

--- Django/myproject/myapp/urls.py
+++ Django/myproject/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.index,name='index'),   # views.py is imported as views and index is the function
-    path("counter/",views.counter,name='counter')    #use / after
+    path('',views.index,name='index'),   # views.py is imported as views and index is the function
+    path('counter',views.counter,name='counter')    #use / after
 ]
--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -6,6 +6,6 @@ def index(request):
     return render(request,'index.html')
 
 def counter(request):
-    text=request.GET['text']  #words is the name of the input textbox 
+    text=request.POST['text']  #words is the name of the input textbox 
     no_of_words=len(text.split())
     return render(request,'counter.html',{'text':text,'no_of_words':no_of_words})
