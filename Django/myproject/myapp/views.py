4952a96fee479b5462a0d8bbf5c49cc924ca13da
Suprim-Karki <shupreem1911@gmail.com>
2025-06-06 17:11:51 +0545

    Getting data and displaying it in another page

--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context={
-        'name':'Suprim',
-        'age':21,
-        'nationality':'Nepalese'
-    }
-    return render(request,'index.html',context)
+    return render(request,'index.html')
+
+def counter(request):
+    text=request.GET['text']  #words is the name of the input textbox 
+    no_of_words=len(text.split())
+    return render(request,'counter.html',{'text':text,'no_of_words':no_of_words})
