1d28822c94c536674d4d0f8acdfbacf1b2b7eca6
Suprim-Karki <shupreem1911@gmail.com>
2025-06-06 13:52:46 +0545

    including myapp.urls in myproject urls using include

--- Django/myproject/myapp/urls.py
+++ Django/myproject/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.index,name=index)   # views.py is imported as views and index is the function
+    path("",views.index,name='index')   # views.py is imported as views and index is the function
 
 ]
