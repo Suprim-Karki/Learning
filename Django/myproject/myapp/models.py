f583df9896e6b438b1f55cb60bc9bc23118a0228
Suprim-Karki <shupreem1911@gmail.com>
2025-06-07 21:19:31 +0545

    models.Charfield used

--- Django/myproject/myapp/models.py
+++ Django/myproject/myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Feature(models.Model):
-    id:int
-    name:str
-    details:str
+class Feature(models.Model): 
+    # id is default here so no need to include
+    name=models.CharField(max_length=100)
+    details=models.CharField(max_length=500)
