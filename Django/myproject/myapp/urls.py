7703aae0330796e95be2fbd603757d7fb017acfd
Suprim-Karki <shupreem1911@gmail.com>
2025-06-09 14:28:26 +0545

    Login page url

--- Django/myproject/myapp/urls.py
+++ Django/myproject/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),   # views.py is imported as views and index is the function
     path('counter',views.counter,name='counter'),
-    path('register',views.register,name='register')
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
 ]
