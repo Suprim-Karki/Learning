8715642ef52af226b39ee088c19e4df20da87271
Suprim-Karki <shupreem1911@gmail.com>
2025-06-18 21:49:45 +0545

    putting TestView in url

--- Django/REST/drfproject/drfproject/urls.py
+++ Django/REST/drfproject/drfproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import Testview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Testview.as_view(),name='test')
 ]
