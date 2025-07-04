aec7767c97f9f80b89809f556cfd66b5c4e7118d
Suprim-Karki <shupreem1911@gmail.com>
2025-07-04 13:10:52 +0545

    Transactions model

--- Django/REST/drfproject/drfapp/models.py
+++ Django/REST/drfproject/drfapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Transactions(models.Model):
+    title=models.CharField(max_length=100)
+    amount=models.FloatField()
+    transaction_type =models.CharField(max_length=100,choices=(("CREDIT","CREDIT"),("DEBIT","DEBIT")))
+
+    #Since we want to make balance +ve when credit and -ve when debit, we overwrite save function
+    def save(self,*args,**kwargs):
+        if self.transaction_type == "DEBIT":
+            self.amount = self.amount* -1
+        return super().save(self,*args,**kwargs)
