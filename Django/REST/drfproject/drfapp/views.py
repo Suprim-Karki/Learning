8be683b460c9522b6dbacb7e0134be3632dd9364
Suprim-Karki <shupreem1911@gmail.com>
2025-07-04 16:30:27 +0545

    Using serializer

--- Django/REST/drfproject/drfapp/views.py
+++ Django/REST/drfproject/drfapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Transactions
 from rest_framework.response import Response
+from .searilizers import TransactionsSerializers
+
 # Create your views here.
 
 def get_transactions(request):
-    query=Transactions.objects.all()
+    queryset =Transactions.objects.all()
+    serializer = TransactionsSerializers(queryset, many=True)
 
-    return Response()
+    return Response({
+        "data":serializer.data
+    })
